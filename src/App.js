f7654a19adb3d3efb46813663f49db1ac4f5072f
Gaurav Gupta <gauravg@eightfold.ai>
2019-04-29 17:32:44 -0700

    User WebKit Speech API to recognize and display voice utterances

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const speechRecognizer = () => {
 
-}
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+       isSpeaking: false,
+       isFetching: false,
+       recognizedText: '',
+       SpeechRecognizer: this.getSpeechRecognizer(),
+    };
+  }
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <h1> Predict the next lyrics! </h1>
-          <div>
-            <i className="fas fa-headphones-alt" />
-          </div>
+  getSpeechRecognizer = () => {
+    const SpeechRecognizer = new window.webkitSpeechRecognition();
+    SpeechRecognizer.continuous = true;
+
+    SpeechRecognizer.onend = () => {
+      this.setState({ isFetching: false });
+    };
+
+    SpeechRecognizer.onresult = (event) => {
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          this.setState({
+            isSpeaking: false,
+            recognizedText: event.results[i][0].transcript,
+          })
+        }
+        SpeechRecognizer.stop();
+      }
+    }
+
+    return SpeechRecognizer;
+  }
+
+
+  getTextFromSpeech = () => {
+    const {
+      isSpeaking,
+      isFetching,
+      recognizedText,
+      SpeechRecognizer,
+     } = this.state;
+
+    this.setState({ isSpeaking: !isSpeaking });
+
+    if (isSpeaking) {
+      SpeechRecognizer.stop();
+    } else {
+      SpeechRecognizer.start();
+    }
+  }
+
+  render() {
+    const {
+      isSpeaking,
+      isFetching,
+      recognizedText,
+      SpeechRecognizer,
+     } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1> Predict the next lyrics! </h1>
+            <div className={`icon ${isSpeaking ? 'voice-input-active' : ''}` } onClick={this.getTextFromSpeech}>
+              <i className="fas fa-microphone-alt" />
+            </div>
+          {
+            recognizedText &&
+            <React.Fragment>
+              <h3> Recognized text </h3>
+              <p> { recognizedText } </p>
+            </React.Fragment>
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 export default App;
