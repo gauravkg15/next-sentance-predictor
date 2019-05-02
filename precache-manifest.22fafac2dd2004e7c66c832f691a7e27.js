9dfcde52154ee85f33ff1ddc927cf8b411afe193
Gaurav Gupta <gauravg@eightfold.ai>
2019-05-02 00:25:11 -0700

    Updates

---
+++ precache-manifest.22fafac2dd2004e7c66c832f691a7e27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2276f1cf1fad4fea1831908b94b8279a",
+    "url": "/next-sentance-predictor/index.html"
+  },
+  {
+    "revision": "362a7255d0e438863945",
+    "url": "/next-sentance-predictor/static/css/main.ffefa2d1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/next-sentance-predictor/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "362a7255d0e438863945",
+    "url": "/next-sentance-predictor/static/js/main.404ea1f8.chunk.js"
+  },
+  {
+    "revision": "3e4388323180bb0bae0e",
+    "url": "/next-sentance-predictor/static/js/runtime~main.c2e0b30e.js"
+  }
+]);
